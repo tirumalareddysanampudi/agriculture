c40e79c00e10f8082c3c7c140c44ed966e052b8a
tirumalareddysanampudi <45302057+tirumalareddysanampudi@users.noreply.github.com>
2025-02-14 20:24:22 +0530

    Delete EmployyeeTaxDeduction directory

--- EmployyeeTaxDeduction/src/main/java/com/app/tax/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.tax.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.tax.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
